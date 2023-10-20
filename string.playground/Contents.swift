fd6d062a8e67ac9882a5f7436ad824d1f83aab62
kesselly kamara <kkesselly30@gmail.com>
2023-10-20 16:46:31 -0700

    day1

--- string.playground/Contents.swift
+++ string.playground/Contents.swift
@@ -11,3 +11,20 @@ let fullname =  fristName + lastName
 
 print("my fullName is : \(fullname)")
  
+
+// prefix and surface and other methods
+
+fullname.hasPrefix("k")
+fullname.hasSuffix("k")
+
+fristName.uppercased()
+fristName.count
+lastName.description
+
+//mult line string : what you see is what you get
+
+let Exam = """
+               my exam will be tomorow
+               and i alway have a pre screeming with Ebay
+"""
+print(Exam)
