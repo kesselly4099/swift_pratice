c238ccfc0ed6000115c606b627bca27eaedad8fe
kesselly kamara <kkesselly30@gmail.com>
2023-10-20 16:26:17 -0700

    day1

---
+++ string.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var Greet = "Hello, playground"
+
+//Concatingnate
+
+let fristName = "kesselly"
+let lastName = "kamara"
+
+let fullname =  fristName + lastName
+
+print("my fullName is : \(fullname)")
+ 
