04f498d1cdbfe28223130214a06f36328d95c14b
kesselly kamara <kkesselly30@gmail.com>
2023-10-20 16:06:11 -0700

    day1

--- weather_and_Temp.playground/Contents.swift
+++ weather_and_Temp.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let day = "monday"
+var currentTemperatuer = 70
+let dailyTemperature = 75
+currentTemperatuer = 80
+let weeklyTemperature = 75
+print("The day of the week is \(day) is sun shine day")
+print("The morning Temperature \(currentTemperatuer)")
 
-print(greeting)
+
