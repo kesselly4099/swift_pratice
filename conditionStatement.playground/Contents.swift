25ed7ba33073dd3816b8d970552566ba28257da4
kesselly kamara <kkesselly30@gmail.com>
2023-10-23 10:57:26 -0700

    day3

--- conditionStatement.playground/Contents.swift
+++ conditionStatement.playground/Contents.swift
@@ -11,7 +11,7 @@ if morning{
 }
 
 
-let greeting = "morning"
+let greeting = "morni"
 
 if greeting == "morning"{
     print("Good morning")
@@ -28,6 +28,19 @@ switch greeting{
 case "morning": print("Good Morning")
 case "Afternoon": print("Good Afternoon")
 default: break
+}
+
+
+let Lang = "Eng"
+let DoB = 2
+
+if Lang == "Eng" && DoB == 24 {
+    print("you are good to good")
+    
+} else if Lang == "Eng" && DoB == 22{
+    print("No we can try to go")
+}else{
+    print("we can do it at all")
 }
 
 
